df9f21852bf29a224c4a62f5eaff2600153ed229
Siddhesh Mangela <sidthecool007@gmail.com>
2017-11-06 11:51:38 +0530

    lookup simplified

--- helpers/lookup.js
+++ helpers/lookup.js
@@ -1,18 +1,10 @@
 function Lookup(elmId) {
-  const targetDataTag = `[data-domr-id="${elmId}"]`;
-  const targetSelector = document.querySelector(targetDataTag);
-  const targetDuplicate = document.querySelectorAll(targetDataTag)[1];
+  const targetSelector = document.querySelector(`[data-domr-id="${elmId}"]`);
 
   if (targetSelector) {
-    if (targetDuplicate) {
-      console.log(`${targetDataTag} Duplicate found`);
-      console.log('data-domr-id ids should be unique');
-    } else {
-      return document.querySelector(`[data-domr-id="${elmId}"]`);
-    }
+    return targetSelector;
   } else {
     console.log(`${elmId} does not exist`);
-    console.log(`Could not find ${targetDataTag}`);
   }
 }
 
