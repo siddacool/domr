5bc92276cace621eca9e626c9500a61caced4a70
Siddhesh Mangela <sidthecool007@gmail.com>
2017-12-28 12:57:13 +0530

    JsUgligy issue fixed

---
+++ resources/js/dom/Domr/distribution/helpers/add-view.js
@@ -0,0 +1,27 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _cloneObject = require('./clone-object');
+
+var _cloneObject2 = _interopRequireDefault(_cloneObject);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function addView(candidate) {
+  var view = candidate.view;
+  var skiplist = ['view'];
+  var routeData = (0, _cloneObject2.default)(candidate, skiplist);
+
+  if (candidate && view) {
+    if (typeof view === 'function') {
+      view(routeData);
+    } else {
+      view;
+    }
+  }
+}
+
+exports.default = addView;
