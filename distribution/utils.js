648e317c38c814d41807fe53bffdef5b807f46e7
Siddhesh Mangela <sidthecool007@gmail.com>
2017-12-28 12:51:13 +0530

    JsUglify issue fixed via converting modules to se5

---
+++ distribution/utils.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _hashLocation = require('./helpers/hash-location');
+
+var _hashLocation2 = _interopRequireDefault(_hashLocation);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var utils = {
+  hashLocation: _hashLocation2.default
+};
+
+exports.default = utils;
